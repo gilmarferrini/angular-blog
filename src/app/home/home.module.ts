ab760308cd69ee96df67fd46039f61b7cb4a853a
gilmarferrini <ferrinigilmar@gmail.com>
2022-01-24 10:04:18 -0300

    feat: adicionado listagem de posts

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { PostsModule } from './posts/posts.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    PostsModule
+  ]
+})
+export class HomeModule { }
