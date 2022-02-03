29cfa605154d2ef4430cd88d4736141745c5fb31
gilmarferrini <ferrinigilmar@gmail.com>
2022-02-03 17:10:45 -0300

    feat: adicionado teste verificando se ao iniciar o dashboard, o usuario sera inicializado

--- src/app/home/dashboard/dashboard.component.spec.ts
+++ src/app/home/dashboard/dashboard.component.spec.ts
@@ -5,8 +5,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardModule } from './dashboard.module';
 import { RouterTestingModule } from '@angular/router/testing';
+import { User } from '../users/models/user';
 
 describe(DashboardComponent.name, () => {
+
+  function buildUser(): User {
+    return {
+      id: 'any_id',
+      avatar: 'any_avatar',
+      username: 'any_username',
+      created_at: new Date()
+    }
+  }
+
 
   let fixture: ComponentFixture<DashboardComponent>
   let component: DashboardComponent
@@ -14,7 +25,6 @@ describe(DashboardComponent.name, () => {
   let tokenService: TokenService
 
   beforeEach(async () => {
-
     await TestBed.configureTestingModule({
       imports: [
         DashboardModule,
@@ -31,6 +41,15 @@ describe(DashboardComponent.name, () => {
 
   it ('Should create component', () => {
     expect(component).toBeTruthy()
+  })
+
+  it ('Should get user when load component', () => {
+    const user = buildUser();
+
+    spyOn(userService, 'getUser').and.returnValue(user)
+    fixture.detectChanges();
+
+    expect(component.user).toBe(user);
   })
 
 })
